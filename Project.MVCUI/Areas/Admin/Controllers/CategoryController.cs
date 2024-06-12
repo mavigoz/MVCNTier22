3879902d457180b115c8c1aafe1f1cac9cdb7d51
emre fidan <emrefidan_763@hotmail.com>
2024-06-12 10:49:38 +0300

    Kategori Ekle Kısmı Oluşturuldu ve Denendi Çalışıyor

--- Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
+++ Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using PagedList;
 using Project.BLL.DesingPattern.GenericRepository.ConctRep;
+using Project.ENTITIES.Models;
 using Project.MVCUI.Areas.Admin.AdminVMClasses;
 using Project.MVCUI.AutentecationClasses;
 using Project.MVCUI.VMClasses;
@@ -48,5 +49,49 @@ namespace Project.MVCUI.Areas.Admin.Controllers
            
            
         }
+        public ActionResult AddCategory()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+ public ActionResult AddCategory(Category category)
+        {
+
+            _cRep.Add(category);
+
+            return RedirectToAction("CategoryList");
+
+        }
+        public ActionResult UpdateCategory(int id)
+        {
+            CategoryAdminVM vM = new CategoryAdminVM { 
+            
+            
+            Category=_cRep.Find(id)
+            };
+
+
+            return View(vM);
+        
+        }
+        [HttpPost]  
+        public ActionResult UpdateCategory(Category category)
+        {
+            _cRep.Update(category);
+            return RedirectToAction("CategoryList");
+
+
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+
+            _cRep.Delete(_cRep.Find(id));
+            return RedirectToAction("CategoryList");
+
+
+        }
+
     }
 }
---
+++ Project.MVCUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Project.BLL.DesingPattern.GenericRepository.ConctRep;
+using Project.BLL.DesingPattern.SingletonPatterns;
+using Project.ENTITIES.Models;
+using Project.MVCUI.Areas.Admin.AdminVMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Areas.Admin.Controllers
+{
+    
+    public class ProductController : Controller
+    {
+        ProductRepository _pRep;
+
+        public ProductController()
+        {
+            _pRep = new ProductRepository() ;        
+        }
+        // GET: Admin/Product
+        public ActionResult ProductList(int? id)
+        {
+            ProductAdminVM vM = id == null ? new ProductAdminVM { Products = _pRep.Where(x => x.ID == id) } : new ProductAdminVM { Products = _pRep.GetAll() };
+            return View(vM);
+        }
+       public ActionResult AddProduct()
+        { return View();
+        
+        
+        
+        
+        }
+        [HttpPost]
+        public ActionResult AddProduc(Product  product)
+        {
+
+            _pRep.Add(product);
+            return RedirectToAction("ProductList");
+
+        }
+        public ActionResult UpdateProduct()
+        { return View(); }
+        public ActionResult UpdateProduct(int id)
+        {
+            ProductAdminVM vM = new ProductAdminVM { 
+            
+            Product=_pRep.Find(id)
+            };
+        
+        return View(vM);
+        
+        }
+        public ActionResult DeleteProduct(int id)
+        {
+
+            _pRep.Delete(_pRep.Find(id));
+
+            return RedirectToAction("ProductList");
+        }
+
+
+    }
+}
